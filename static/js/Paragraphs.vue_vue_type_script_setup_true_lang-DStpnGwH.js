fe99a98a95b0ece5c1f83d94e753bd25f69bef5a
QuadnucYard <50077758+QuadnucYard@users.noreply.github.com>
2024-06-12 12:23:22 +0000

    Deploying to gh-pages from @ QuadnucYard/graduating-csw@37e30dcc714429e3c42b223fdc3fafc00fa61de3 🚀

--- static/js/Paragraphs.vue_vue_type_script_setup_true_lang-DStpnGwH.js
+++
@@ -1,2 +0,0 @@
-import{d as l,f as u,o as a,c as o,F as f,D as x,E as g}from"./index-PKrEBFVo.js";const h=l({__name:"Paragraphs",props:{data:{}},setup(c,{expose:p}){const i=c,n=u();return p({getText:()=>{var t;return(t=n.value)==null?void 0:t.innerText},getTexts:()=>{const t=[],r=n.value;if(!r)return t;for(const s of r.childNodes.values()){const e=s.textContent;e&&e.trim().length>0&&t.push(e.trim())}return t}}),(t,r)=>(a(),o("div",{ref_key:"block",ref:n,class:"paragraphs"},[(a(!0),o(f,null,x(i.data.split(`
-`),(s,e)=>(a(),o("p",{key:e},g(s),1))),128))],512))}});export{h as _};
